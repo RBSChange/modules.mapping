624885a110204b5e46a4317f25a77864f1ad7c16
Gaël PORT <gael.port@rbs.fr>
2010-11-17 10:37:46 +0100

    [DELETE] Delete old unused file.

--- lib/AreaListTreeParser.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class mapping_AreaListTreeParser extends tree_parser_XmlListTreeParser
-{
-    public function getTree($componentId = null, $offset = 0, $order = null, $filter = null)
-    {
-    	$request = Context::getInstance()->getRequest();
-    	return parent::getTree($request->getParameter(K::COMPONENT_ID_ACCESSOR), $offset, $order, $filter);
-    }
-}
